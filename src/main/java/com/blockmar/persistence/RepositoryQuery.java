84f9ddc16e6b4fa794fc64fe7692f148b4ebccd7
Anders Blockmar <blockmar+github@gmail.com>
2012-07-01 16:33:43 +0200

    Added offset to Query

--- src/main/java/com/blockmar/persistence/RepositoryQuery.java
+++ src/main/java/com/blockmar/persistence/RepositoryQuery.java
@@ -1,11 +1,12 @@
 package com.blockmar.persistence;
 
-
 public interface RepositoryQuery<T> {
 
 	public abstract T first();
 
 	public abstract RepositoryQueryResult<T> result();
+
+	public abstract <V> RepositoryQuery<T> offset(int offset);
 
 	public abstract <V> RepositoryQuery<T> and(String key, V value);
 
